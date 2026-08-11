1cf3725064be2e9e7f0997e0762411007914d6b1
edenyang01-del <edenyang01@gmail.com>
2026-08-11 13:40:37 +0800

    Upgrade targetSdk and compileSdk to 35 for Google Play

--- src/data/androidProjectFiles.ts
+++ src/data/androidProjectFiles.ts
@@ -14,12 +14,12 @@ export const androidProjectFiles: AndroidFile[] = [
 
 android {
     namespace = "com.bilingo.radio"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.bilingo.radio"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
     }
