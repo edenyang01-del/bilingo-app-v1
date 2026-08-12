25215664f506ed84d54d05e925cc13522f06d572
edenyang01-del <edenyang01@gmail.com>
2026-08-12 18:30:05 +0800

    chore: bump version to 1.9.3
    Update application version and user agent string for release 1.9.3.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.bilingo.radio"
         minSdk = 24
         targetSdk = 35
-        versionCode = 192
-        versionName = "1.9.2"
+        versionCode = 193
+        versionName = "1.9.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- android/app/src/main/java/com/bilingo/radio/ui/screens/MainScreen.kt
+++ android/app/src/main/java/com/bilingo/radio/ui/screens/MainScreen.kt
@@ -132,7 +132,7 @@ fun MainScreen(
                         loadWithOverviewMode = false
                         setSupportZoom(false)
                         textZoom = 100
-                        userAgentString = "$userAgentString AndroidApp/1.9.2"
+                        userAgentString = "$userAgentString AndroidApp/1.9.3"
                         cacheMode = WebSettings.LOAD_DEFAULT
                     }
 
